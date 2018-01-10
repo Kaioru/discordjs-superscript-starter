fdfc29a7a0383425c4b8669a5e6c7d09452cc257
Kaioru <keifuurawrs@gmail.com>
2018-01-10 20:20:41 +0800

    Add gulp clean tasks

--- tasks/default.js
+++ tasks/default.js
@@ -1,9 +1,18 @@
+var del = require('del');
+
 module.exports = function (gulp, options, plugins) {
     gulp.task('watch', function () {
         plugins.watch('app/**/*', function () {
+            gulp.start('clean');
             gulp.start('build');
         });
     });
 
-    gulp.task('default', ['build']);
+    gulp.task('clean', function () {
+        return del(['dist']);
+    });
+
+    gulp.task('default', ['clean'], function () {
+        gulp.start('build');
+    });
 }
